220466c99aad145028710d201d3c386df8766c3b
val982-two <valerafilaev982@gmail.com>
2026-09-15 21:58:04 +0300

    Lab 1

--- Sort/A.cpp
+++ Sort/A.cpp
@@ -4,26 +4,28 @@
 using namespace std;
 
 void SelectionSort(vector<int>& arr, int n) {
-    for (int i = 0; i < n - 1; i++){
-        for (int j = i + 1; j < n; j++){
-            if (arr[i] <= arr[j]){
-                swap(arr[i], arr[j]);
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[min_idx]) {
+                min_idx = j;
             }
         }
+        swap(arr[i], arr[min_idx]);
     }
 }
 
 int main() {
     vector<int> arr;
     int x;
-    while (cin >> x){
+    while (cin >> x) {
         arr.push_back(x);
     }
 
     int n = arr.size();
     SelectionSort(arr, n);
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     return 0;
